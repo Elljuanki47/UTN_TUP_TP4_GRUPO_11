a315a2527c36309c2ad22e4a03745c1325ed97be
Elljuanki47 <jizarate47@outlook.com>
2026-09-13 16:54:11 -0300

    Inicializo el segundo ejercicio 2 con la gried view y la conexion a la bd

--- TP4_GRUPO_11/Ejercicio2.aspx.cs
+++ TP4_GRUPO_11/Ejercicio2.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TP4_GRUPO_11
 {
     public partial class Ejercicio2 : System.Web.UI.Page
     {
+        private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                SqlConnection conexion = new SqlConnection(cadenaConexion);
+                conexion.Open();
 
+                string consulta = "SELECT * FROM Productos";
+
+                SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexion);
+
+                DataSet ds = new DataSet();
+                adaptador.Fill(ds, "Productos");
+
+                gvProductos.DataSource = ds.Tables["Productos"];
+                gvProductos.DataBind();
+
+                conexion.Close()
+            }
         }
     }
 }
